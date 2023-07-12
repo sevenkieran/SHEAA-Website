46760a145c467a97e67fa454d8cff109f217365a
sevenkieran <sevenkieran@gmail.com>
2023-07-12 10:56:30 -0400

    about model

--- website/__init__.py
+++ website/__init__.py
@@ -4,8 +4,11 @@ from flask import Flask
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = "ahsdgfkjaghdfkj"
+
     from .views import views
+    from .AboutModel import AboutModel
 
     app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(AboutModel, url_prefix="/")
 
     return app
